171f4c4f0261e36a40c87d8e61d41799f1c9e8bc
antoniooreany <antonio_oreany@mail.ru>
2019-04-19 21:37:03 +0200

    subroutines read_KEY0, read_KEY3 syncronized by adding checks to each of them: if r9!=0 => foto read_KEY*

--- Lab1_Ass3.s
+++ Lab1_Ass3.s
@@ -4,6 +4,7 @@
 	.text
 START:
 	call read_KEY0		# subroutine read_KEY0 is called
+	call read_KEY3		# subroutine read_KEY3 is called
 	br START		# check for the key pressed again
 	
 endloop:
@@ -14,6 +15,15 @@ read_KEY0:
 	movia r2, 0x840		# r2 <- 0x840
 	ldw r3, (r2)		# r3 <- (0x840)
 	andi r9, r3, 0x1	# r9 <- masked value of (0x840)
-	ret
+	bne r9, r0, read_KEY0	# if r9!=0 => foto read_KEY0
+	ret			# return
 	
+read_KEY3:
+	movia r2, 0x840		# r2 <- 0x840
+	ldw r3, (r2)		# r3 <- (0x840)
+	andi r9, r3, 0x8	# r9 <- masked value of (0x840)
+	bne r9, r0, read_KEY3	# if r9!=0 => foto read_KEY3
+	ret			# return
 
+
+	
